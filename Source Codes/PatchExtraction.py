b2633cb4c0e544702cefa2d4c47d0b753c31ec50
kamruleee51 <kamruleeekuet@gmail.com>
2019-07-31 02:41:01 +0200

    Update PatchExtraction.py

--- Source Codes/PatchExtraction.py
+++ Source Codes/PatchExtraction.py
@@ -13,8 +13,9 @@ import numpy as np
 from skimage import data, color, io, img_as_float
 import cv2
 import os 
+from sklearn.feature_extraction.image import extract_patches_2d
 
-cwd = 'D:\KiTS Project\kits19\scripts'
+PatchSavePath = 'D:\KiTS Project\kits19\scripts'
 
 dataPath = glob.glob('D:\KiTS Project\kits19\scripts\dataTrain\*');
 dataPath.sort()
@@ -24,71 +25,19 @@ for i in range(len(dataPath)):
     org_vol = (nib.load(dataPath[i]+'\imaging.nii.gz')).get_fdata()
     
     mask_vol = (nib.load(dataPath[i]+'\segmentation.nii.gz')).get_fdata()
-
-    kidney = mask_vol.copy()
-    tumor = mask_vol.copy()
-
-    kidney[kidney!=1]=0
-    kidney[kidney==1]=255
-    tumor[tumor!=2]=0
-    tumor[tumor==2]=255
-
-
-#    for k in range (300,400,1):
-    for k in range (len(org_vol[:,1,1])):
-        
-
-
-        img_color = np.dstack((org_vol[k,:,:], org_vol[k,:,:], org_vol[k,:,:]))
-
-        kidney_color = np.zeros_like(img_color)
-        kidney_color[kidney[k,:,:]!=0] = [0, 0, 255] # Blue block
-        
-        tumor_color = np.zeros_like(img_color)
-        tumor_color[tumor[k,:,:]!=0] = [0, 255, 0] # Blue block
-        
-        img_hsv_T = color.rgb2hsv(img_color)
-        img_hsv_K = img_hsv_T.copy()
-        kidney_color = color.rgb2hsv(kidney_color)
-        tumor_color = color.rgb2hsv(tumor_color)
-        
-        img_hsv_K[:,:, 0] = kidney_color[:,:, 0]
-        img_hsv_K[:,:, 1] = kidney_color[:,:, 1] * 0.6
-        img_hsv_K = color.hsv2rgb(img_hsv_K)
-        
-        path = cwd + '\\ImageChecker\\' + dataPath[i][-10:] +'_'+str(k)+'_kidney_over.png'
-        
-        cv2.imwrite(path,img_hsv_K)
-
-        img_hsv_T[:,:, 0] = tumor_color[:,:, 0]
-        img_hsv_T[:,:, 1] = tumor_color[:,:, 1] * 0.6
-        img_hsv_T = color.hsv2rgb(img_hsv_T)
-
-        path = cwd + '\\ImageChecker\\' + dataPath[i][-10:] +'_'+str(k)+'_tumor_over.png'
-        cv2.imwrite(path,img_hsv_T)
-        
-        
-        
-        path = cwd + '\\ImageChecker\\' + dataPath[i][-10:] +'_'+str(k)+'_kidney_.png'
-        
-        cv2.imwrite(path,kidney[k,:,:])
-        
-        path = cwd + '\\ImageChecker\\' + dataPath[i][-10:] +'_'+str(k)+'_tumor_.png'
-        cv2.imwrite(path,tumor[k,:,:])
-        
-        path = cwd + '\\ImageChecker\\' + dataPath[i][-10:] +'_'+str(k)+'_mask_.png'
-        cv2.imwrite(path,120*mask_vol[k,:,:])
-        
-        path = cwd + '\\ImageChecker\\' + dataPath[i][-10:] +'_'+str(k)+'_org_.png'
-        cv2.imwrite(path,org_vol[k,:,:])
-        
-        
-        
-        print('Case-> '+str(i)+'   '+'slice-> '+str(k))
-        
-#    if i==5:
-#    break
     
+    for k in range (300,400,1):
+        current_slice_org = org_vol[k,:,:]
+        current_slice_mask = mask_vol[k,:,:]
+        patches_current_slice_org = extract_patches_2d(current_slice_org, (256, 256))
+        patches_current_slice_mask= extract_patches_2d(current_slice_mask, (256, 256))
+        break
+    
+    
+    
+    break
+
+
     
     
 
