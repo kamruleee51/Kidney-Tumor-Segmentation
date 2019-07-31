e8fde41b922a03e527714c1ede9921cd3889f2f5
kamruleee51 <kamruleeekuet@gmail.com>
2019-07-31 03:13:58 +0200

    Updated

--- Source Codes/PatchExtraction.py
+++ Source Codes/PatchExtraction.py
@@ -30,14 +30,14 @@ for i in range(len(dataPath)):
     
     mask_vol = (nib.load(dataPath[i]+'\segmentation.nii.gz')).get_fdata()
     
-#    for k in range (300,400,1):
-    for k in range (len(org_vol[:,1,1])):
+    for k in range (300,400,1):
+#    for k in range (len(org_vol[:,1,1])):
         current_slice_org = org_vol[k,:,:]
         current_slice_mask = mask_vol[k,:,:]
         patches_current_slice_org = extract_patches_2d(current_slice_org, (256, 256),
-                                        max_patches=NumberPatch)
+                                        max_patches=NumberPatch,random_state=1)
         patches_current_slice_mask= extract_patches_2d(current_slice_mask, (256, 256),
-                                                       max_patches=NumberPatch)
+                                                       max_patches=NumberPatch,random_state=1)
         
         for j in range(NumberPatch):
             path = PatchSavePath + '\\Patches\\' + dataPath[i][-10:]+'_'+str(k)+str(j)+'_org_.png'
@@ -47,8 +47,13 @@ for i in range(len(dataPath)):
             cv2.imwrite(path,120*patches_current_slice_mask[j,:,:])
             
             kidney_overlay, tumor_overlay=mask_overlay_org(patches_current_slice_org[j,:,:],patches_current_slice_mask[j,:,:])
+            path = PatchSavePath + '\\Patches\\' + dataPath[i][-10:]+'_'+str(k)+str(j)+'_kidney_overlay_.png'
+            cv2.imwrite(path,kidney_overlay)
             
-#        break
+            path = PatchSavePath + '\\Patches\\' + dataPath[i][-10:]+'_'+str(k)+str(j)+'_tumor_overlay_.png'
+            cv2.imwrite(path,tumor_overlay)
+            
+        break
     
     
     
