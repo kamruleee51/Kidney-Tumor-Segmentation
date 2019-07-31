a294d670d8504f20e7f0d8e9192119abf81d665f
kamruleee51 <kamruleeekuet@gmail.com>
2019-07-31 03:18:43 +0200

    Update PatchExtraction.py

--- Source Codes/PatchExtraction.py
+++ Source Codes/PatchExtraction.py
@@ -19,7 +19,7 @@ from utilsFunc import mask_overlay_org
 
 PatchSavePath = 'D:\KiTS Project\kits19\scripts'
 
-NumberPatch =500
+NumberPatch =100
 
 dataPath = glob.glob('D:\KiTS Project\kits19\scripts\dataTrain\*');
 dataPath.sort()
@@ -30,8 +30,8 @@ for i in range(len(dataPath)):
     
     mask_vol = (nib.load(dataPath[i]+'\segmentation.nii.gz')).get_fdata()
     
-    for k in range (300,400,1):
-#    for k in range (len(org_vol[:,1,1])):
+#    for k in range (300,400,1):
+    for k in range (len(org_vol[:,1,1])):
         current_slice_org = org_vol[k,:,:]
         current_slice_mask = mask_vol[k,:,:]
         patches_current_slice_org = extract_patches_2d(current_slice_org, (256, 256),
@@ -40,24 +40,24 @@ for i in range(len(dataPath)):
                                                        max_patches=NumberPatch,random_state=1)
         
         for j in range(NumberPatch):
-            path = PatchSavePath + '\\Patches\\' + dataPath[i][-10:]+'_'+str(k)+str(j)+'_org_.png'
+            path = PatchSavePath + '\\Patches\\' + dataPath[i][-10:]+'_'+str(k)+'_'+str(j)+'_org_.png'
             cv2.imwrite(path,patches_current_slice_org[j,:,:])
             
-            path = PatchSavePath + '\\Patches\\' + dataPath[i][-10:]+'_'+str(k)+str(j)+'_mask_.png'
+            path = PatchSavePath + '\\Patches\\' + dataPath[i][-10:]+'_'+str(k)+'_'+str(j)+'_mask_.png'
             cv2.imwrite(path,120*patches_current_slice_mask[j,:,:])
             
             kidney_overlay, tumor_overlay=mask_overlay_org(patches_current_slice_org[j,:,:],patches_current_slice_mask[j,:,:])
-            path = PatchSavePath + '\\Patches\\' + dataPath[i][-10:]+'_'+str(k)+str(j)+'_kidney_overlay_.png'
+            path = PatchSavePath + '\\Patches\\' + dataPath[i][-10:]+'_'+str(k)+'_'+str(j)+'_kidney_overlay_.png'
             cv2.imwrite(path,kidney_overlay)
             
-            path = PatchSavePath + '\\Patches\\' + dataPath[i][-10:]+'_'+str(k)+str(j)+'_tumor_overlay_.png'
+            path = PatchSavePath + '\\Patches\\' + dataPath[i][-10:]+'_'+str(k)+'_'+str(j)+'_tumor_overlay_.png'
             cv2.imwrite(path,tumor_overlay)
             
+
+    
+    
+    if i==5:
         break
-    
-    
-    
-    break
 
 
     
